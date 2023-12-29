30acd7c0dac0b6610583abb4bffa32df22d5139a
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2023-12-29 21:50:15 +0545

    Updated

---
+++ 2 Data types/DataType.js
@@ -0,0 +1,29 @@
+// there are 8 data types in jvascript
+
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. Object
+// 8 Bigint
+
+
+// 1. Strings
+var color = "Yellow";
+let name = "Sonam";
+const surName = "Tamang";
+
+
+// 2. Numbers
+var class = 11;
+let length = 8;
+const id = 7;
+
+// 3. Boolean
+let x = true;
+let y = false;
+
+// 4. Undefined
+var undefinedVar;
