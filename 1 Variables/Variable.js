67565c32ef4e29895c2dd7e2347c23a300a5c877
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2023-12-29 21:38:49 +0545

    Updated

---
+++ 1 Variables/Variable.js
@@ -0,0 +1,14 @@
+// Variables are containers for storing data 
+//in javasript whet is 3 type of varrible for define and store specific dataypes
+
+// var
+// this is most use to define varribles before es6 
+var name = "Sonam";
+
+// let 
+// this is mostly use and realble for defining varriable type which can change dynamically
+let surName = "Tamang";
+
+// const 
+// const  use to define costant Variables which can not be change
+const fullName = "Sonam Tamang";
