b08e08d1b2fbdc9c2c776c5946fc7cbc06d7137b
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2024-01-06 21:37:57 +0545

    Updated

---
+++ 4 Loops/Loops.js
@@ -0,0 +1,24 @@
+JavaScript supports several types of loops, which are control flow structures that allow you to repeatedly execute a block of code. Here are the main types of loops in JavaScript:
+
+1. for Loop
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  
+
+2. while Loop
+
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+
+3. do while Loop
+
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
