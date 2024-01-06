c933d70849a5487a38d32d5dbe6f275dd8349f56
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2024-01-06 21:27:25 +0545

    Updated

---
+++ 3 Operators/Operators.js
@@ -0,0 +1,39 @@
+// an operator is a special symbol or keyword that performs operations on one or more operands. Operators are used to manipulate variables and values, combining them or transforming them in some way. The operands can be variables, literals, or expressions.
+
+// There is 7 types of operatiors in js
+
+// 1. Arithmetic Operators:
+// 2. Assignment Operators:
+// 3. Comparison Operators:
+// 4. Logical Operators:
+// 5. Unary Operators:
+// 6. Conditional (Ternary) Operator:
+// 7. Bitwise Operators:
+
+// 1. Arithmetic Operators:
+
+// + (Addition)
+// - (Subtraction)
+// * (Multiplication)
+// / (Division)
+// % (Modulus - returns the remainder)
+// ** (Exponentiation)
+
+let a = 10;
+let b = 3;
+console.log(a + b); // 13
+console.log(a - b); // 7
+console.log(a * b); // 30
+console.log(a / b); // 3.333...
+console.log(a % b); // 1
+console.log(a ** b); // 1000
+
+// 2. Assignment Operators:
+= (Assignment)
++= (Add and assign)
+-= (Subtract and assign)
+*= (Multiply and assign)
+/= (Divide and assign)
+
+let x = 5;
+x += 3; // equivalent to x = x + 3;
