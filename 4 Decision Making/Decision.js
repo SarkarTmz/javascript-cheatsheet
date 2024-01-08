5d58558cfaa2206e7b87164dd4ceb44d68d9efdf
SarkarTmz <105831207+SarkarTmz@users.noreply.github.com>
2024-01-08 21:14:26 +0545

    Updated

---
+++ 4 Decision Making/Decision.js
@@ -0,0 +1,27 @@
+
+// Decision-making in JavaScript is often achieved through conditional statements and loops. Here are some common constructs for decision-making in JavaScript:
+
+
+// 1 if statement:
+// 2  else if statement:
+// 3 switch statement:
+// 4 ternary operator:
+
+// 1 if statement:
+// The if statement is used to execute a block of code if a specified condition evaluates to true.
+if (condition) {
+    // Code to be executed if the condition is true
+} else {
+    // Code to be executed if the condition is false
+}
+
+// ****
+
+let x = 10;
+
+if (x > 5) {
+    console.log("x is greater than 5");
+} else {
+    console.log("x is not greater than 5");
+}
+
